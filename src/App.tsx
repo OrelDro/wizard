e22d81e3b94e264e6cdf9f97ce8bbe8bac382665
Oreld <oreld89@gmail.com>
2021-11-22 21:55:25 +0200

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const wizard = {
+  name: "My wizard",
+  sections: [
+    {
+      name: "Section 1", steps: [
+        { name: "step 1", question: "What is your name?", type: "string" },
+        { name: "step 2", question: "What is your age?", type: "number" },
+        { name: "last step", question: "gender?", type: "multi", values: ["MALE", "FEMALE"] },
+      ]
+    },
+    {
+      name: "Section 2",
+      steps: [
+        { name: "another step", question: "What shirt size do you wear?", type: "multi", values: ["XS", "S", "M", "L", "XL", "XXL"] }
+      ]
+    },
+    {
+      name: "Section 3",
+      steps: [
+        { name: "another step3", question: "What shirt size do you wear 3333?", type: "multi", values: ["XS", "S", "M", "L", "XL", "XXL"] }
+      ]
+    }
+  ]
+}
+
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
